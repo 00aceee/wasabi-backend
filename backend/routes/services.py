5a3fec140e35ffc044ba42ac028651e17e29eb97
Ace <leonardomarkace.pdm@gmail.com>
2025-11-11 16:54:26 +0800

    images

--- backend/routes/services.py
+++ backend/routes/services.py
@@ -1,17 +1,15 @@
+from flask import Blueprint, jsonify, send_from_directory, request
 import os
-from flask import Blueprint, jsonify, send_from_directory
 
 services_bp = Blueprint("services", __name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-REACT_PUBLIC_PATH = os.path.join(BASE_DIR, "../../marmu-react/public/assets")
+REACT_PUBLIC_PATH = os.path.join(BASE_DIR, "../../backend/public/assets")
 
-# Serve static images directly
 @services_bp.route('/assets/<path:filename>')
 def serve_assets(filename):
     return send_from_directory(REACT_PUBLIC_PATH, filename)
 
-# Return list of tattoo + haircut images
 @services_bp.route("/images", methods=["GET"])
 def get_service_images():
     tattoo_folder = os.path.join(REACT_PUBLIC_PATH, "tattoo_images")
@@ -24,7 +22,8 @@ def get_service_images():
         for filename in os.listdir(folder):
             if filename.lower().endswith(".png"):
                 name = os.path.splitext(filename)[0].replace("_", " ").replace("-", " ").title()
-                image_url = f"http://localhost:5000/assets/{service_type}_images/{filename}"
+                # Use request.host_url instead of localhost
+                image_url = f"{request.host_url}api/services/assets/{service_type}_images/{filename}"
                 images.append({"name": name, "image": image_url})
         return sorted(images, key=lambda x: x["name"])
 
